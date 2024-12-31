5023df2c7f7cc26121964fade6b936f4f1a90f8f
Hassan Mehmood <hm350074@gmail.com>
2024-12-31 17:44:25 +0500

    make build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Ensure this matches your Output Directory on Vercel
+  },
   plugins: [react()],
   assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.svg', '**/*.JPG'], // Add other formats if needed
 });
